51e0a8215726dfba9ff10b490be9fbb05040317a
Chelle77322 <serenynadarra@gmail.com>
2021-04-08 23:44:34 +0930

    Adding routes

---
+++ routes/api-routes.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.get("/api/workouts", (request, result) => {
+  db.Keeping_Fit.find({})
+    .sort({ date: -1 })
+    .then((workouts) => {
+      result.status(200).json(workouts);
+    })
+    .catch((error) => {
+      result.status(400).json(error);
+    });
+});
+
+router.get("/api/workouts/range", (request, result) => {
+  db.Keeping_Fit.find({})
+    .sort({ date: -1 })
+    .then((workouts) => {
+      result.status(200).json(workouts);
+    })
+    .catch((error) => {
+      result.status(400).json(error);
+    });
+});
+
+router.post("/api/workouts", (request, result) => {
+  db.Keeping_Fit.create(request.body)
+    .then((workouts) => {
+      result.status(201).json(workouts);
+    })
+    .catch((error) => {
+      result.status(400).json(error);
+    });
+});
+
+router.put("/api/workouts/:id", async (request, result) => {
+  const id = request.params.id;
+  const body = request.body;
+  db.Keeping_Fit.updateOne(
+    { _id: id },
+    {
+      $push: {
+        exercises: { ...body },
+      },
+    }
+  )
+    .then((workouts) => {
+      result.status(200).json(workouts);
+    })
+    .catch((error) => {
+      result.status(400).json(error);
+    });
+});
+
+module.exports = router;
---
+++ routes/html-routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const path = require("path");
+
+router.get("/", (request, result) => {
+  result.sendFile(path.join(__dirname + "/index.html"));
+});
+
+router.get("/stats", (request, result) => {
+  result.sendFile(path.join(__dirname + "/../public/stats.html"));
+});
+
+router.get("/exercise", (request, result) => {
+  result.sendFile(path.join(__dirname + "/../public/exercise.html"));
+});
+
+module.exports = router;
