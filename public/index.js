553e9d55d2e4d2fbfcf51de8ec992902d671890e
Chelle77322 <serenynadarra@gmail.com>
2021-04-15 21:32:40 +0930

    Still returns as undefined

--- public/index.js
+++ public/index.js
@@ -2,10 +2,10 @@ init();
 
 async function init(){
   if (location.search.split("=")[1] === undefined) {
-    const summaryWorkouts = await callAPI.getprevWorkout();
-    console.log(summaryWorkouts);
-if (summaryWorkouts) {
-      location.search = "?id=" + summaryWorkouts._id;
+    const workout = await callAPI.getprevWorkout(workoutData);
+  
+if (workout) {
+      location.search = "?id=" + workout_id;
     } else {
       document.querySelector("#continue-btn").classList.add("d-none")
     }
