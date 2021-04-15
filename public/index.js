147d45703e68ace0bd1b9f20f60bbea0a82e3fc6
Chelle77322 <serenynadarra@gmail.com>
2021-04-15 21:35:38 +0930

    Fixing error

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ init();
 
 async function init(){
   if (location.search.split("=")[1] === undefined) {
-    const workout = await callAPI.getprevWorkout(workoutData);
+    const workout = await callAPI.getprevWorkout();
   
 if (workout) {
       location.search = "?id=" + workout_id;
