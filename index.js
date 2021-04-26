ed981b988610829c5485195b1d6d6100d6618315
Chelle77322 <serenynadarra@gmail.com>
2021-04-26 13:24:10 +0930

    Working on fixing write to database

--- index.js
+++ index.js
@@ -1,57 +1,37 @@
-//install packages
+//install  npm packages
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression');
-const routes = require ('./routes/html-routes');
-const dotenv = require("dotenv");
-dotenv.config();
 
+//set port
+const PORT = process.env.PORT || 3333;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Keeping_Fit";
+
+//app settings
 const app = express();
-//parser
+//use logger
+app.use(logger("dev"));
+
+//parsers
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(compression());
 
-//set port
-const PORT = process.env.PORT || 3333;
+const dotenv = require("dotenv");
+dotenv.config();
 
-//use logger
-app.use(logger("dev"));
-
-
-
+//Mongoose connection
+mongoose.connect(MONGODB_URI, {
+    userNewUrlParser: true,
+    useFindAndModify: false
+});
+app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes"));
 
 //use static files
-
 app.use(express.static(__dirname + "/public/"));
-
-//const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@workouts.a1ska.mongodb.net/Keeping_Fit?retryWrites=true&w=majority`
-
-//mongoose.connect({uri_decode_auth: true}+connectionString);
-
-
-//mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/keeping_fit:${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}`, {useNewUrlParser: true});
-
-const db = db("Keeping_fit");
-
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/keeping_fit`, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-});
-mongoose.Promise = Promise;
-//require('./seed/seed');
-require ('./routes/html-routes')(app);
-app.use(routes);
-
-//use routes
-//app.use(require('./routes/api-routes'));
-//app.use(require('./routes/html-routes'));
-require('./routes/api-routes')(app);
-
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}..`);
-})
+});
