69728f804d1fdae8cc2e6aa0597313b576742c8c
Chelle77322 <serenynadarra@gmail.com>
2021-04-27 21:23:20 +0930

    Complete rewrite of index file to fix MongoDB localhost connection issue

--- index.js
+++ index.js
@@ -1,38 +1,19 @@
-//install  npm packages
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require('compression');
-
-const dotenv = require("dotenv");
-dotenv.config();
-
-//set port
 const PORT = process.env.PORT || 3333;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/keeping_fit";
 
-//app settings
 const app = express();
-//use logger
-//app.use(logger("dev"));
 
-//parsers
-//app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.statis("public"));
 
-//app.use(express.urlencoded({ extended: true }));
-//app.use(express.json());
+//ROUTES
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes"));
 
-//use static files
-//app.use(express.static("public"));
-
-//Mongoose connection
-mongoose.connect(MONGODB_URI, {
+//MONGO CONNECTION
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
     useNewUrlParser: true,
-    useFindAndModify: false
 });
-
-
-
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}..`);
-});
+app.listen(PORT, ()=> console.log(`App is now listening ${PORT}`));
