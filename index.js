5ec9c19189d32ee4a91f3dbcb2a32ea6b6a03a3b
Chelle77322 <serenynadarra@gmail.com>
2021-04-28 12:44:04 +0930

    Removing - in file names

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const PORT = process.env.PORT || 3333;
 
 const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static("public"));
 
 //ROUTES
-app.use(require("./routes/api-routes.js"));
-app.use(require("./routes/html-routes"));
+app.use(require("./routes/apiroutes.js"));
+app.use(require("./routes/htmlroutes"));
 
 //MONGO CONNECTION
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
     useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 app.listen(PORT, ()=> console.log(`App is now listening ${PORT}`));
---
+++ routes/htmlroutes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const path = require("path");
+
+//Call to get the index page with a try and catch
+router.get("/", (request, result)=>{
+  result.sendFile(path.join(__dirname, "public/index.html"));
+});
+//Call to get the exercise page with a try and catch
+app.get("/exercise" , (request, result) => {
+  
+  result.sendFile(path.join(__dirname , "public/exercise.html"));
+});
+
+//Call to get the stats page with a try and catch
+app.get("/stats", (request, result) => {
+  
+  result.sendFile(path.join(__dirname,  "public/stats.html"));
+});
+
+module.exports = router;
