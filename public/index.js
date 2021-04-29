7e67762c51ed62ab3baf5cf9a6fec51de9868644
Chelle77322 <serenynadarra@gmail.com>
2021-04-29 20:39:27 +0930

    Need to fix function call for last workout

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ init();
 
 async function init(){
   if (location.search.split("=")[1] == undefined) {
-    const workouts = await API.getPrevWorkout();
+    const workouts = await API.getLastWorkout();
     console.log(workouts);
   
 if (workouts) {
