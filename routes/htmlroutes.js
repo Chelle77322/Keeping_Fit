a55297e5c91b34d83f43d4107a9fcdbb62523054
Chelle77322 <serenynadarra@gmail.com>
2021-06-04 16:43:16 +0930

    working out killing

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,14 +2,14 @@ const path = require("path");
 
 module.exports = function (app) {
     app.get("/", function(request, result) {
-        result.sendFile(path.join(__dirname, "../public/index.html"));
+        result.sendFile(path.join(__dirname, "/index.html"));
       });
     
       app.get("/exercise", function(request, result) {
-        result.sendFile(path.join(__dirname, "../public/exercise.html"));
+        result.sendFile(path.join(__dirname, "/exercise.html"));
       });
 
       app.get("/stats", function(request, result) {
-        result.sendFile(path.join(__dirname, "../public/stats.html"));
+        result.sendFile(path.join(__dirname, "/stats.html"));
       });
 }
