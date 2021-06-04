283141d2b24ef7cdb194e6d243aee53ba5147bb4
Chelle77322 <serenynadarra@gmail.com>
2021-06-04 13:04:47 +0930

    Troubleshooting rendering issues

--- seed/seed.js
+++ seed/seed.js
@@ -1,81 +1,78 @@
-let mongoose = require('mongoose');
-let db = require("../models");
+let mongoose = require("mongoose");
+let Workout = require("../models/workouts");
 
-//mongoose connection rewrite here
-mongoose.connect("mongodb://localhost/workouts",{
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+module.exports = function(){
+  var d = new Date();
+  
 
-let seedWorkouts = [
+
+let workoutSeed = [
   {
-    day: new Date().setDate(new Date().getDate()-10),
+    date: new Date(new Date().setDate(new Date().getDate()-8)),
+    totalDuration: 50,
+    day: dayCalc(8),
     exercises: [
       {
         type: "resistance",
-        name: "Lateral Pulldown",
-        duration: 10,
-        weight: 75,
-        reps: 15,
-        sets: 3
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-9),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Shoulder Press",
-        duration: 7,
-        weight: 45,
+        name: "Bicep Curl",
+        duration: 30,
+        weight: 100,
         reps: 10,
         sets: 4
       }
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-8),
+    date: new Date(new Date().setDate(new Date().getDate()-7)),
+    totalDuration: 20,
+    day: dayCalc(7),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Lateral Pull",
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    date: new Date(new Date().setDate(new Date().getDate()-6)),
+    totalDuration: 25,
+    day: dayCalc(6),
     exercises: [
       {
         type: "resistance",
         name: "Push Press",
-        duration: 15,
+        duration: 25,
         weight: 185,
-        reps: 5,
-        sets: 2
+        reps: 8,
+        sets: 4
       }
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-7),
+    date: new Date(new Date().setDate(new Date().getDate()-5)),
+    totalDuration: 125,
+    day: dayCalc(5),
     exercises: [
       {
         type: "cardio",
         name: "Running",
-        duration: 25,
-        distance: 4,
-      },
-      {
-        type: "cardio",
-        name: "Jogging",
-        duration: 60,
-        distance: 10
-      },
-      {
-        type: "cardio",
-        name: "Sprint Intervals",
-        duration: 15,
-        distance: 2
+        duration: 125,
+        distance: 4
       }
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-6),
+    date: new Date(new Date().setDate(new Date().getDate()-4)),
+    totalDuration: 20,
+    day: dayCalc(4),
     exercises: [
       {
         type: "resistance",
-        name: "Bench Press",
+        name: "Sumo Squats",
         duration: 20,
         weight: 285,
         reps: 10,
@@ -84,25 +81,29 @@ let seedWorkouts = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-5),
-    exercise: [
+    date: new Date(new Date().setDate(new Date().getDate()-3)),
+    totalDuration: 20,
+    day: dayCalc(3),
+    exercises: [
       {
         type: "resistance",
-        name: "Hamstring Curl",
-        duration: 10,
-        weight: 140,
-        reps: 5,
-        sets: 3
+        name: "Face Pulls",
+        duration: 20,
+        weight: 120,
+        reps: 10,
+        sets: 4
       }
     ]
   },
   {
-    day: new Date(new Date().setDate(new Date().getDate() - 4)),
-    exercise: [
+    date: new Date(new Date().setDate(new Date().getDate()-2)),
+    totalDuration: 30,
+    day: dayCalc(2),
+    exercises: [
       {
         type: "resistance",
-        name: "Leg Press",
-        duration: 10,
+        name: "Quad Press",
+        duration: 30,
         weight: 300,
         reps: 10,
         sets: 4
@@ -110,21 +111,25 @@ let seedWorkouts = [
     ]
   },
   {
-    day: new Date(new Date().setDate(new Date().getDate() - 3)),
-    exercise: [
+    date: new Date(new Date().setDate(new Date().getDate()-1)),
+    totalDuration: 20,
+    day: dayCalc(1),
+    exercises: [
       {
         type: "resistance",
-        name: "Bent Over Row",
-        duration: 5,
-        weight: 20,
+        name: "Hammer Curls",
+        duration: 20,
+        weight: 30,
         reps: 10,
         sets: 4
       }
     ]
   },
   {
-    day: new Date(new Date().setDate(new Date().getDate() - 2)),
-    exercise: [
+    date: new Date(new Date().setDate(new Date().getDate()-8)),
+    totalDuration: 20,
+    day: dayCalc(8),
+    exercises: [
       {
         type: "resistance",
         name: "Military Press",
@@ -134,16 +139,47 @@ let seedWorkouts = [
         sets: 4
       }
     ]
+  },
+  {
+    date: new Date(new Date().setDate(new Date().getDate()-3)),
+    totalDuration: 30,
+    day: dayCalc(3),
+    exercises: [
+      {
+        type: "cardio",
+        name: "Swimming",
+        duration: 30,
+        distance: 2
+      }
+    ]
   }
 ];
 
+Workout.deleteMany({})
+  .then(() => Workout.insertMany(workoutSeed))
+  .then(data => {
+    console.log(data.length + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
 
-db.workouts.deleteMany({})
- .then(() => db.workouts.insertMany(seedWorkouts))
- .then(exercise => {
-   console.log(exercise.result.n + " records inserted!");
-process.exit(0);
- }).catch(error => {
-console.error(error);
- process.exit(1);
-});
+}
+
+function dayCalc(num){
+  var d = new Date();
+  num = num % 7;
+  var sub = d.getDay() - num;
+
+  if(sub >= 0){
+    return sub
+  }
+  else{
+    sub += 7;
+    return sub;
+  }
+  
+
+}
